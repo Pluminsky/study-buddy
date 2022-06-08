4865767da88aca373a3a8e554a8d1ba743c67f21
Tomek <tomaszsliwinskipriv@gmail.com>
2022-06-08 23:30:53 +0200

    Added button events

--- src/components/moleculs/UsersListItem/UsersListItem.js
+++ src/components/moleculs/UsersListItem/UsersListItem.js
@@ -3,16 +3,23 @@ import PropTypes from 'prop-types';
 import { Wrapper, StyledAverage, StyledInfo } from './UserListItem.style';
 import Button from 'components/atoms/Button/Button';
 
-const UsersListItem = ({ userData: { average, name, attendance = '0%' } }) => (
-  <Wrapper>
-    <StyledAverage value={average}>{average}</StyledAverage>
-    <StyledInfo>
-      <p>{name}</p>
-      <p>attendance: {attendance}</p>
-    </StyledInfo>
-    <Button></Button>
-  </Wrapper>
-);
+const UsersListItem = ({
+  index,
+  userData: { average, name, attendance = '0%' },
+}) => {
+  const showIndex = (index) => alert(`This is student ${index + 1} index`);
+
+  return (
+    <Wrapper>
+      <StyledAverage value={average}>{average}</StyledAverage>
+      <StyledInfo>
+        <p>{name}</p>
+        <p>attendance: {attendance}</p>
+      </StyledInfo>
+      <Button onClick={() => showIndex(index)} />
+    </Wrapper>
+  );
+};
 
 UsersListItem.propTypes = {
   userData: PropTypes.shape({
